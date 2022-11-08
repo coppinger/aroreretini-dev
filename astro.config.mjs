c4f880dc0e2410ff39a5b5313c0bb58d344b25da
Charlie Coppinger <charles.coppinger@gmail.com>
2022-11-08 17:16:35 +1300

    change astro config to remove ssr stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,4 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify(),
 });
