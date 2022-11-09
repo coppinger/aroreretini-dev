dc3727fed40e200b6b0809c18c83e83c38b4fabd
Charlie Coppinger <charles.coppinger@gmail.com>
2022-11-10 12:03:42 +1300

    update layout and add swup transitions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  safelist: [
-    "lg:grid-cols-[1fr_2px_4fr]",
-    "lg:grid-cols-[1fr_2px_3fr_2px_1fr]",
-  ],
+  safelist: ["lg:grid-cols-[1fr_2px_4fr]", "lg:grid-cols-[3fr_2px_1fr]"],
   theme: {
     extend: {
       fontFamily: {
